e11d88a36daad9a48ddd1a4fe1aa2e7a2374648c
Javier <javier.parra@correounivalle.edu.co>
2022-03-10 08:33:23 -0500

    primer commit

---
+++ webapp-service/src/main/java/com/univalle/cataya/CatayaWebApplication.java
@@ -0,0 +1,14 @@
+package com.univalle.cataya;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatayaWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatayaWebApplication.class, args);
+	}
+
+
+}
